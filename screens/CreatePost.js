b653f7edf31097c9ac72e71753d7b0f71930265b
mercycommits <101704718+mercycommits@users.noreply.github.com>
2023-04-01 18:30:29 +0530

    Update CreatePost.js

--- screens/CreatePost.js
+++ screens/CreatePost.js
@@ -33,58 +33,7 @@ export default class CreatePost extends Component {
         this.fetchUser();
     }
 
-    async addPost() {
-        if (
-            this.state.caption
-        ) {
-            let postData = {
-                preview_image: this.state.previewImage,
-                caption: this.state.caption,
-                author: firebase.auth().currentUser.displayName,
-                created_on: new Date(),
-                author_uid: firebase.auth().currentUser.uid,
-                profile_image: this.state.profile_image,
-                likes: 0
-            };
-            await firebase
-                .database()
-                .ref(
-                    "/posts/" +
-                    Math.random()
-                        .toString(36)
-                        .slice(2)
-                )
-                .set(postData)
-                .then(function (snapshot) { });
-            this.props.setUpdateToTrue();
-            this.props.navigation.navigate("Feed");
-        } else {
-            Alert.alert(
-                "Error",
-                "All fields are required!",
-                [{ text: "OK", onPress: () => console.log("OK Pressed") }],
-                { cancelable: false }
-            );
-        }
-    }
-
-    async fetchUser() {
-        let theme, name, image;
-        await firebase
-            .database()
-            .ref("/users/" + firebase.auth().currentUser.uid)
-            .on("value", function (snapshot) {
-                theme = snapshot.val().current_theme;
-                name = `${snapshot.val().first_name} ${snapshot.val().last_name}`;
-                image = snapshot.val().profile_picture;
-            });
-        this.setState({
-            light_theme: theme === "light" ? true : false,
-            name: name,
-            profile_image: image
-        });
-    }
-
+    
     render() {
         let preview_images = {
             image_1: require("../assets/image_1.jpg"),
